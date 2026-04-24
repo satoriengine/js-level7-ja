e73cc690bc76dabf1dacf3403266fcff6a66cbd1
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2026-04-24 13:18:00 +0000

    fix chapter06 for main

--- chapter06/work03.js
+++ chapter06/work03.js
@@ -1,19 +1,1 @@
 // クラス変数による状態管理
-class User {
-    // インスタンス変数
-    name;
-    // クラス変数;
-    // クラス全体で1つだけのカウンター
-    static count = 0;
-
-    constructor(name) {
-        this.name = name;
-        // インスタンスが作られるたびに、クラス変数を加算
-        User.count++;
-    }
-}
-
-new User('Alice');
-new User('Bob');
-
-console.log('current user num is ', User.count); // 2
